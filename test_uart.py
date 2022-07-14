833d669314c6f6d1100d54b5dab6ce8f332bed20
Yannick Molle <yannick.molle@onsemi.com>
2022-07-14 14:07:03 +0200

    First commit

--- test_uart.py
+++ test_uart.py
@@ -8,7 +8,7 @@ serial = Serial("/dev/ttymxc2", 115200)
 # Read up to 128 bytes with 500ms timeout
 buf = serial.read(128, 0.5)
 i = 500
-while buf:
+while True:
     print("read {:d} bytes: _{:s}_".format(len(buf), buf))
     buf = serial.read(128, 0.5)
     i = i-1
