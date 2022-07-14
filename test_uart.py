c0d8e79dde7aa82f66c88f0fc820d5785bbaadfc
Yannick Molle <yannick.molle@onsemi.com>
2022-07-14 14:02:11 +0200

    First commit

--- test_uart.py
+++ test_uart.py
@@ -1,7 +1,7 @@
 from periphery import Serial
 
 # Open /dev/ttyUSB0 with baudrate 115200, and defaults of 8N1, no flow control
-serial = Serial("/dev/ttymxc0", 115200)
+serial = Serial("/dev/ttymxc2", 115200)
 
 #serial".write(b"Hello World!")
 
